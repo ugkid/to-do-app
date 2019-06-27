44be1349ed22ac4a9efd2b9e578e5e083ebccfda
Michael Bahari <bahayana@gmail.com>
2019-06-27 21:31:04 +1000

    added checkboxes to App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,17 @@
 import React from "react";
+
+function App() {
+  return (
+    <div>
+      <input type="checkbox" />
+      <p>something</p>
+
+      <input type="checkbox" />
+      <p>something</p>
+
+      <input type="checkbox" />
+      <p>something</p>
+    </div>
+  );
+}
+export default App;
