a424c1cea652a8eaafbec7820de4bc9f57c6fea9
Michael Bahari <bahayana@gmail.com>
2019-07-01 23:17:21 +1000

    changed app component into stateless class component, loaded todosData into state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 import TodoItem from "./TodoItem";
 import todosData from "./todosData";
 
-function App() {
-  const todoItems = todosData.map(item => (
-    <TodoItem key={item.id} item={item} />
-  ));
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: todosData
+    };
+  }
 
-  return <div className="todo-list">{todoItems}</div>;
+  render() {
+    const todoItems = this.state.todos.map(item => (
+      <TodoItem key={item.id} item={item} />
+    ));
+
+    return <div className="todo-list">{todoItems}</div>;
+  }
 }
 
 export default App;
