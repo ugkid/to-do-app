11b1a7badf75637771d38a97c3b27b5384743055
Michael Bahari <bahayana@gmail.com>
2019-07-02 21:10:55 +1000

    added event handler and state

--- src/App.js
+++ src/App.js
@@ -10,9 +10,23 @@ class App extends Component {
     };
   }
 
+  handleChange = id => {
+    this.setState(prevState => {
+      const updatedTodos = prevState.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      });
+      return {
+        todos: updatedTodos
+      };
+    });
+  };
+
   render() {
     const todoItems = this.state.todos.map(item => (
-      <TodoItem key={item.id} item={item} />
+      <TodoItem key={item.id} item={item} handleChange={this.handleChange} />
     ));
 
     return <div className="todo-list">{todoItems}</div>;
